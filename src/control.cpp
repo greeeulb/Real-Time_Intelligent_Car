eb96b8a368cf06710a867fe8b1687c5a7863c1a0
PhillipsSong3 <98277076+PhillipsSong3@users.noreply.github.com>
2022-04-20 00:04:44 +0100

    Add files via upload

--- src/control.cpp
+++ src/control.cpp
@@ -1,90 +1,53 @@
 #include <stdio.h>
 #include "pcatest.h"
 #include "motortest.h"
+#include <QMenuBar>
+#include <QKeyEvent>
 
 // keyPressEvent() rebuild
 void control::keyPressEvent(QKeyEvent *event)
 {
     if(!event->isAutoRepeat())
     {
-        if(mKeyFlag)//whether press ctrl
-        {
-            switch (event->key()) //determine which button get pressed
-            {
-            case Qt::Key_Left:
-                on_BtnCcw_clicked();
-                break;
-            case Qt::Key_Right:
-                on_BtnCw_clicked();
-                break;
-            }
-        }
-        else
-        {
+       
             switch (event->key()) //determine which button get pressed
             {
             case Qt::Key_Up:
-                on_BtnGo_clicked();
+                SetMotorMode1(2000,2000,2000,2000);
                 break;
             case Qt::Key_Down:
-                on_BtnBack_clicked();
+                setMotorMode1(-2000,-2000,-2000,-2000);  
                 break;
             case Qt::Key_Left:
-                on_BtnLeft_clicked();
+                 SetMotorMode1(-500,-500,2000,2000) ;
                 break;
             case Qt::Key_Right:
-                on_BtnRight_clicked();
-                break;
-            case Qt::Key_Z:
-                on_BtnSpeedUp_clicked();
-                break;
-            case Qt::Key_X:
-                on_BtnSpeedDown_clicked();
-                break;
-            case Qt::Key_Control:	//if pressed Ctrl, set mKeyFlag to true
-                mKeyFlag = true;
+                SetMotorMode1(2000,2000,-500,-500); 
                 break;
             }
-        }
+        
     }
 }
-// keyReleaseEvent()rebuild
-void MainWindow::keyReleaseEvent(QKeyEvent *event)
+
+void control::keyReleaseEvent(QKeyEvent *event)
 {
     if(!event->isAutoRepeat())
     {
-        if(mKeyFlag)//whether pressed ctrl
-        {
-            switch (event->key()) //determine which button get loosed
-            {
-            case Qt::Key_Left:
-                getStraight();
-                break;
-            case Qt::Key_Right:
-                getStraight();
-                break;
-            case Qt::Key_Control:
-                mKeyFlag = false;	//if loose Ctrl, set mKeyFlag to false
-                break;
-            }
-        }
-        else
-        {
             switch (event->key()) //determine which button get loosed
             {
             case Qt::Key_Up:
-                on_BtnStop_clicked();
+                SetMotorMode1(0,0,0,0);         
                 break;
             case Qt::Key_Down:
-                on_BtnStop_clicked();
+               SetMotorMode1(0,0,0,0);
                 break;
             case Qt::Key_Left:
-                getStraight();
+               SetMotorMode1(0,0,0,0);
                 break;
             case Qt::Key_Right:
-                getStraight();
+              SetMotorMode1(0,0,0,0);
                 break;
             }
-        }
+        
     }
 }
